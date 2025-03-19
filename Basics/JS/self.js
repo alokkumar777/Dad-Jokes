25cdb175603aa683ba411a815385e0daf9db8e61
Alok Kumar <136910791+alokkumar777@users.noreply.github.com>
2025-03-19 10:25:22 +0530

    update

--- Basics/JS/self.js
+++ Basics/JS/self.js
@@ -1,4 +1,13 @@
-// Question, logical
-
-
-
+function isPalindrome(str) {
+    let userString = str.split(" ").join("").toLowerCase();
+    let reverseString = userString.split("").reverse().join("");
+  
+    if (userString == reverseString) {
+      return `String is Palindrome`;
+    } else {
+      return `String is not Palindrome`;
+    }
+  }
+  
+  console.log(isPalindrome('A man a plan a canal Panama'));
+  
