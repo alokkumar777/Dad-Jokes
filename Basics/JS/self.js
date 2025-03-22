93c7e1305a703909a767168bfd4304f1d14d5fee
Alok Kumar <136910791+alokkumar777@users.noreply.github.com>
2025-03-22 21:44:43 +0530

    Update

--- Basics/JS/self.js
+++ Basics/JS/self.js
@@ -1,21 +1,45 @@
-function newConnections() {
-  let newConn = [
-    ["G", "A", "L", "V"],
-    ["J", "K", "M", "N", "P"],
-  ];
+// const prompt = require("prompt-sync");
 
-  for (let row of newConn) {
-    for (let frnd of row) {
-      console.log(frnd);
+// Ask user what to perform
+// 1. new
+// 2. list
+// 3. del
+// 4. quit
+
+console.log(`
+  *****************************************
+  Type the following to perform some action
+  - New : For new task
+  - List : To see the list of task
+  - Delete : To del a task
+  - Quit : For quit the app
+  *****************************************
+  `);
+
+let taskList = [];
+// console.log(userInput);
+
+while (true) {
+  let userInput = prompt("What you want to perform?").toLowerCase();
+  // first case
+  if (userInput === "new") {
+    let task = prompt("Add your task");
+    taskList.push(task);
+    console.log("Task added");
+    // second case
+  } else if (userInput === "list") {
+    for (let [index, tasks] of taskList.entries()) {
+      console.log(`${index}: ${tasks}`);
     }
-    console.log("----------");
-    // console.log(row)
+    // third case
+  } else if (userInput === "delete") {
+    let enterIndex = prompt("Enter index of task to delete");
+    taskList.splice(enterIndex, 1);
+    console.log("Tasked deleted");
+  } else if (userInput === "quit") {
+    console.log("Quiting app");
+    break;
+  } else {
+    alert("Enter valid input! Try again.");
   }
 }
-
-// calling
-newConnections();
-
-
-
-
