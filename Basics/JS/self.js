3523eef0c26dd5a9ff9bf49fabd1bf4cdbd172d0
Alok Kumar <136910791+alokkumar777@users.noreply.github.com>
2025-03-22 12:21:54 +0530

    Update

--- Basics/JS/self.js
+++ Basics/JS/self.js
@@ -1,11 +1,20 @@
-let newConn = [
-  ["Guddu", "Alisha", "Lakshay", "Vidya"],
-  ["Jai", "Kalyani", "Mannan", "Naina", "Pragya"],
-];
+function newConnections() {
+  let newConn = [
+    ["Guddu", "Alisha", "Lakshay", "Vidya"],
+    ["Jai", "Kalyani", "Mannan", "Naina", "Pragya"],
+  ];
 
-for (row of newConn) {
-  for (frnd of row) {
-    console.log(frnd);
+  for (let row of newConn) {
+    for (let frnd of row) {
+      console.log(frnd);
+    }
+    console.log("----------");
+    // console.log(row)
   }
-  console.log('----------')
 }
+
+// calling
+newConnections();
+console.log(row)
+
+
