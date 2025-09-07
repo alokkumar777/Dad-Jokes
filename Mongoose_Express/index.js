a7d0a6f1155ca27c09fb32e6c69810049a2afc1b
Alok Kumar <136910791+alokkumar777@users.noreply.github.com>
2025-09-07 23:00:52 +0530

    edit option added

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
 const Product = require("./models/product");
 // Server setup
@@ -20,13 +21,16 @@ mongoose
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.use(express.json());
 
+// show new product
 app.get("/products/new", (req, res) => {
   res.render("products/new");
 });
 
+// add new product
 app.post("/products", async (req, res) => {
   const { name, price, category } = req.body;
   const newProduct = new Product({ name, price, category });
@@ -35,15 +39,34 @@ app.post("/products", async (req, res) => {
   res.redirect("/products");
 });
 
+// show all product list
 app.get("/products", async (req, res) => {
   const products = await Product.find({});
   res.render("products/index", { products });
 });
 
+// get particular product full detail
 app.get("/products/:id", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
   res.render("products/show", { product });
+});
+
+// edit or update the exiting product
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product });
+});
+
+app.put("/products/:id", async (req, res) => {
+  const { name, price, category } = req.body;
+  await Product.findByIdAndUpdate(
+    req.params.id,
+    { name, price, category },
+    { new: true, runValidators: true }
+  );
+  res.redirect("/products");
 });
 
 app.listen(PORT, () => {
