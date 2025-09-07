5d68dadbbf32d8fb2adec0279a91ac1cb9ed9a9d
Alok Kumar <136910791+alokkumar777@users.noreply.github.com>
2025-09-07 16:35:40 +0530

    add product route

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -19,8 +19,21 @@ mongoose
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
+
+app.get("/products/new", (req, res) => {
+  res.render("products/new");
+});
+
+app.post("/products", async (req, res) => {
+  const { name, price, category } = req.body;
+  const newProduct = new Product({ name, price, category });
+
+  await newProduct.save();
+  res.redirect("/products");
+});
 
 app.get("/products", async (req, res) => {
   const products = await Product.find({});
